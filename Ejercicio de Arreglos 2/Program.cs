de0e323ed56876a113a338cd027fad04779b4819
ManuelGrijalva <mgrijalvatenas@gmail.com>
2023-04-11 11:35:58 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio de Arreglos 2/Program.cs
@@ -0,0 +1,27 @@
+﻿
+void arreglos()
+{
+    String[] nombre = new string[]
+{
+    "Fernanda", "Melvin", "Luis", "Brandon", "Yamileth"
+};
+
+    int[] notas = new int[]
+    {
+    70,90,43,85,30
+    };
+
+    int notaMasAlta = notas[0];
+    int indiceNotaMasAlta = 0;
+
+    for (int i = 0; i < notas.Length; i++)
+    {
+        if (notas[i] > notaMasAlta)
+        {
+            notaMasAlta = notas[i];
+            indiceNotaMasAlta = i;
+        }
+    }
+    Console.WriteLine($"La nota más alta es del alumn@ {nombre[indiceNotaMasAlta]} con una nota de: {notaMasAlta}");
+};
+arreglos();
